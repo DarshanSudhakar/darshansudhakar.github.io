74c02fa7871484db7ffa2da91862bcef4f22d9cb
Darshan Sudhakar <darshanraj.shetty@gmail.com>
2021-05-22 23:31:30 +0530

    Giving a personal touch by changing the user name (#2)
    * Changed content from Anurag Hazara to Darshan Sudhakar
    
    * Adding the Devicon svg files (#1)
    
    This was done as Devicon-React integration was not so seamless
    
    Co-authored-by: DarshanSudhakar <$K3Ug1i&>
    
    Co-authored-by: DarshanSudhakar <$K3Ug1i&>

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,6 +8,11 @@ import Flex from '@src/components/common/Flex';
 
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
+import DotNet from '@src/static/tech-svg/dot-net-plain-wordmark.svg';
+import CSharp from '@src/static/tech-svg/csharp-plain.svg';
+import VisualStudio from '@src/static/tech-svg/visualstudio-plain.svg';
+import SQLServer from '@src/static/tech-svg/microsoftsqlserver-plain-wordmark.svg';
+
 const SkillsWrapper = styled.section`
   ${props => props.theme.spacing.sectionBottom};
   position: relative;
@@ -46,15 +51,15 @@ const WordClouds = () => {
       aria-hidden="true"
       className="skills__word-clouds"
     >
-      <p style={{ top: 50, left: '100%', fontSize: 23 }}>HTML5</p>
-      <p style={{ top: 0, left: 0, fontSize: 25 }}>WebGL</p>
-      <p style={{ top: 200, left: -60, fontSize: 14 }}>CSS3</p>
-      <p style={{ top: '30%', left: '35%', fontSize: 18 }}>figma</p>
-      <p style={{ top: 40, left: '75%', fontSize: 12 }}>antd</p>
-      <p style={{ top: 380, left: '85%', fontSize: 18 }}>MongoDB</p>
-      <p style={{ top: 320, left: '65%', fontSize: 18 }}>Gatsby</p>
-      <p style={{ top: 350, left: 150, fontSize: 20 }}>ES6</p>
-      <p style={{ top: 120, left: '22%', fontSize: 12 }}>blender</p>
+      <p style={{ top: 50, left: '100%', fontSize: 23 }}>.NET</p>
+      <p style={{ top: 0, left: 0, fontSize: 18 }}>Azure Devops</p>
+      <p style={{ top: 200, left: -60, fontSize: 14 }}>MS Test</p>
+      <p style={{ top: '30%', left: '35%', fontSize: 25 }}>C#</p>
+      <p style={{ top: 40, left: '75%', fontSize: 12 }}>WCF</p>
+      <p style={{ top: 380, left: '85%', fontSize: 18 }}>SQL Server</p>
+      <p style={{ top: 320, left: '65%', fontSize: 18 }}>Javascript</p>
+      <p style={{ top: 350, left: 150, fontSize: 20 }}>Winforms</p>
+      <p style={{ top: 120, left: '22%', fontSize: 12 }}>REST</p>
     </Parallax>
   );
 };
@@ -66,10 +71,14 @@ const Skills = () => (
 
       {/* <SkillPie percent={90} name={"JavaScript"} /> */}
       <Flex className="skill__icons" justify="space-around" align="center">
-        <FontAwesomeIcon icon={['fab', 'html5']} size="5x" />
+        {/* <FontAwesomeIcon icon={['fab', 'html5']} size="5x" />
         <FontAwesomeIcon icon={['fab', 'js']} size="5x" />
         <FontAwesomeIcon icon={['fab', 'react']} size="5x" />
-        <FontAwesomeIcon icon={['fab', 'node']} size="5x" />
+        <FontAwesomeIcon icon={['fab', 'node']} size="5x" /> */}
+        <DotNet style = {{width: 150}}/>
+        <CSharp style = {{width: 150}}/>
+        <VisualStudio style = {{width: 150}}/>
+        <SQLServer style = {{width: 150}}/>
       </Flex>
 
       <Hidden md xs sm>
